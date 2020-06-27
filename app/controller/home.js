91f5b81b797fc2b4fd553903f1c994257849c149
张啸天 <zhangxiaotian@xiaoduotech.com>
2020-06-27 22:24:08 +0800

    feat: add

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,20 +5,24 @@ class HomeController extends Controller {
     const { ctx } = this;
     global.host = `${ctx.request.protocol}://${ctx.request.host}`;
     global.href = ctx.request.href;
+
     ctx.logger.info(111111)
 
     const render = require('../public/umi.server');
+
     ctx.logger.info(22222)
 
     const { err, html } = await render({
       path: ctx.request.url,
     });
+
     ctx.logger.info(33333)
 
     if (err) {
       ctx.status = 404;
       return;
     }
+
     ctx.logger.info(44444)
 
     ctx.body = html;
